6a2a142378800ff2ceb9d8c7e5499d5ef361d638
Alireza Montazeri <montazeri.alireza65@gmail.com>
2022-02-05 00:36:02 -0500

    responsive layout added

--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,52 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:merchant_app/widgets/navigation_bar/navigation_bar_tablet_desktop.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'navigation_bar_mobile.dart';
 
 class MyNavigationBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            height: 80,
-            width: 150,
-            child: Image.asset('assets/logo.png'),
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              _NavBarItem('Home'),
-              SizedBox(
-                width: 60,
-              ),
-              _NavBarItem('Category'),
-              SizedBox(
-                width: 60,
-              ),
-              _NavBarItem('Shopping Cart'),
-              SizedBox(
-                width: 60,
-              ),
-              _NavBarItem('My Info')
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _NavBarItem extends StatelessWidget {
-  final String title;
-
-  const _NavBarItem(this.title);
+  const MyNavigationBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: const TextStyle(fontSize: 18),
+    return ScreenTypeLayout(
+      mobile: NavigationBarMobile(),
+      tablet: NavigationBarTabletDesktop(),
     );
   }
 }
---
+++ lib/widgets/navigation_bar/navigation_bar_tablet_desktop.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'navbar_item.dart';
+import 'navbar_logo.dart';
+
+class NavigationBarTabletDesktop extends StatelessWidget {
+  const NavigationBarTabletDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const NavBarLogo(),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              NavBarItem('Home'),
+              SizedBox(
+                width: 60,
+              ),
+              NavBarItem('Category'),
+              SizedBox(
+                width: 60,
+              ),
+              NavBarItem('Shopping Cart'),
+              SizedBox(
+                width: 60,
+              ),
+              NavBarItem('My Info')
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
