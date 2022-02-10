8eb43d04cc4b49c2b1e2a28b51770f35cc933884
Set <93010230+zhiyi-dong@users.noreply.github.com>
2022-02-10 17:54:35 -0500

    Update category_page_list_view.dart

--- lib/widgets/category_page_list_view/category_page_list_view.dart
+++ lib/widgets/category_page_list_view/category_page_list_view.dart
@@ -17,69 +17,96 @@ class _CategoryPageListViewState extends State<CategoryPageListView> {
     return Container(
       margin: const EdgeInsets.all(16),
       child: Flexible(
-        child: GridView(
+        child: ListView(
           physics: const NeverScrollableScrollPhysics(),
           shrinkWrap: true,
           children: [
-            Text(
-              "Books:",
-              style: TextStyle(fontSize: 25),
+            Text("Books:", style: TextStyle(fontSize: 25)),
+            Container(
+              child: GridView(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5, mainAxisSpacing: 40, crossAxisSpacing: 20),
+                children: [
+                  CategoryPageListItem(
+                      "book 1", "\$10", "Description of book 1", "assets/book.jpeg"),
+                  CategoryPageListItem(
+                      "book 2", "\$20", "Description of book 2", "assets/book.jpeg"),
+                  CategoryPageListItem(
+                      "book 3", "\$30", "Description of book 3", "assets/book.jpeg"),
+                  CategoryPageListItem(
+                      "book 4", "\$40", "Description of book 4", "assets/book.jpeg"),
+                  CategoryPageListItem(
+                      "book 5", "\$50", "Description of book 5", "assets/book.jpeg"),
+                ],
+              ),
+
             ),
-            CategoryPageListItem(
-                "book 1", "\$10", "Description of book 1", "assets/book.jpeg"),
-            CategoryPageListItem(
-                "book 2", "\$20", "Description of book 2", "assets/book.jpeg"),
-            CategoryPageListItem(
-                "book 3", "\$30", "Description of book 3", "assets/book.jpeg"),
-            CategoryPageListItem(
-                "book 4", "\$40", "Description of book 4", "assets/book.jpeg"),
-            CategoryPageListItem(
-                "book 5", "\$50", "Description of book 5", "assets/book.jpeg"),
-            Text(
-              "Clothing:",
-              style: TextStyle(fontSize: 25),
+            Text("Clothing:", style: TextStyle(fontSize: 25)),
+            Container(
+              child: GridView(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5, mainAxisSpacing: 40, crossAxisSpacing: 20),
+                children: [
+                  CategoryPageListItem(
+                      "clothing 1", "\$10", "Description of clothing 1", "assets/clothes.jpeg"),
+                  CategoryPageListItem(
+                      "clothing 2", "\$20", "Description of clothing 2", "assets/clothes.jpeg"),
+                  CategoryPageListItem(
+                      "clothing 3", "\$30", "Description of clothing 3", "assets/clothes.jpeg"),
+                  CategoryPageListItem(
+                      "clothing 4", "\$40", "Description of clothing 4", "assets/clothes.jpeg"),
+                  CategoryPageListItem(
+                      "clothing 5", "\$50", "Description of clothing 5", "assets/clothes.jpeg"),
+                ],
+              ),
+
             ),
-            CategoryPageListItem("cloth 1", "\$10", "Description of cloth 1",
-                "assets/clothes.jpeg"),
-            CategoryPageListItem("cloth 2", "\$20", "Description of cloth 2",
-                "assets/clothes.jpeg"),
-            CategoryPageListItem("cloth 3", "\$30", "Description of cloth 3",
-                "assets/clothes.jpeg"),
-            CategoryPageListItem("cloth 4", "\$40", "Description of cloth 4",
-                "assets/clothes.jpeg"),
-            CategoryPageListItem("cloth 5", "\$50", "Description of cloth 5",
-                "assets/clothes.jpeg"),
-            Text(
-              "Furniture:",
-              style: TextStyle(fontSize: 25),
+            Text("Furniture:", style: TextStyle(fontSize: 25)),
+            Container(
+              child: GridView(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5, mainAxisSpacing: 40, crossAxisSpacing: 20),
+                children: [
+                  CategoryPageListItem("furniture 1", "\$10",
+                      "Description of furniture 1", "assets/furniture.jpeg"),
+                  CategoryPageListItem("furniture 2", "\$20",
+                      "Description of furniture 2", "assets/furniture.jpeg"),
+                  CategoryPageListItem("furniture 3", "\$30",
+                      "Description of furniture 3", "assets/furniture.jpeg"),
+                  CategoryPageListItem("furniture 4", "\$40",
+                      "Description of furniture 4", "assets/furniture.jpeg"),
+                  CategoryPageListItem("furniture 5", "\$50",
+                      "Description of furniture 5", "assets/furniture.jpeg"),
+                ],
+              ),
+
             ),
-            CategoryPageListItem("furniture 1", "\$10",
-                "Description of furniture 1", "assets/furniture.jpeg"),
-            CategoryPageListItem("furniture 2", "\$20",
-                "Description of furniture 2", "assets/furniture.jpeg"),
-            CategoryPageListItem("furniture 3", "\$30",
-                "Description of furniture 3", "assets/furniture.jpeg"),
-            CategoryPageListItem("furniture 4", "\$40",
-                "Description of furniture 4", "assets/furniture.jpeg"),
-            CategoryPageListItem("furniture 5", "\$50",
-                "Description of furniture 5", "assets/furniture.jpeg"),
-            Text(
-              "Electronics:",
-              style: TextStyle(fontSize: 25),
+            Text("Electronics:", style: TextStyle(fontSize: 25)),
+            Container(
+              child: GridView(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5, mainAxisSpacing: 40, crossAxisSpacing: 20),
+                children: [
+                  CategoryPageListItem("electronic 1", "\$10",
+                      "Description of electronic 1", "assets/electronic.jpeg"),
+                  CategoryPageListItem("electronic 2", "\$20",
+                      "Description of electronic 2", "assets/electronic.jpeg"),
+                  CategoryPageListItem("electronic 3", "\$30",
+                      "Description of electronic 3", "assets/electronic.jpeg"),
+                  CategoryPageListItem("electronic 4", "\$40",
+                      "Description of electronic 4", "assets/electronic.jpeg"),
+                  CategoryPageListItem("electronic 5", "\$50",
+                      "Description of electronic 5", "assets/electronic.jpeg"),
+                ],
+              ),
+
             ),
-            CategoryPageListItem("electronic 1", "\$10",
-                "Description of electronic 1", "assets/electronic.jpeg"),
-            CategoryPageListItem("electronic 2", "\$20",
-                "Description of electronic 2", "assets/electronic.jpeg"),
-            CategoryPageListItem("electronic 3", "\$30",
-                "Description of electronic 3", "assets/electronic.jpeg"),
-            CategoryPageListItem("electronic 4", "\$40",
-                "Description of electronic 4", "assets/electronic.jpeg"),
-            CategoryPageListItem("electronic 5", "\$50",
-                "Description of electronic 5", "assets/electronic.jpeg"),
+
           ],
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 6, mainAxisSpacing: 160, crossAxisSpacing: 10),
         ),
       ),
     );
