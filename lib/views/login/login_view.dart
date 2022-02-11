6f67944fadb6bbb9bfa30945063535151ac122df
Alim Manjiyani <manjiyani.alim@gmail.com>
2022-02-10 21:56:35 -0500

    new commit

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:merchant_app/widgets/login_view/login_page_view.dart';
-import 'package:merchant_app/widgets/signup_view/signup_page_view.dart';
+import 'package:merchant_app/widgets/login_view/login_page_view.dart';
+// import 'package:merchant_app/widgets/signup_view/signup_page_view.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -12,7 +12,7 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return const SignupPageView();
-    //return const LoginPageView();
+    //return const SignupPageView();
+    return const LoginPageView();
   }
 }
