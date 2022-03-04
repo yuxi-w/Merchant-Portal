6c9afc88fcb46425940b4d05007f5202242a319a
Yuxi W <48072303+yuxi-w@users.noreply.github.com>
2022-03-04 12:16:14 -0500

    Remove redundant files in the branch

--- lib/constants/colors/app_colors.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primaryColor = const Color.fromARGB(200, 0, 114, 160);
--- lib/routing/route_names.dart
+++
@@ -1,5 +0,0 @@
-const String HomeRoute = 'home';
-const String CategoryRoute = 'category';
-const String ShoppingCartRoute = 'shoppingCart';
-const String PersonalInfoRoute = 'personalInfo';
-const String LoginRoute = 'login';
